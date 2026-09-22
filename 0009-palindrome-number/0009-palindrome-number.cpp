3f2482394499dc40be964ec7fb4ff46d549a2610
Disha Chougule <dishachougule05@gmail.com>
2026-09-22 11:16:10 +0530

    Time: 3 ms (43.85%), Space: 8.5 MB (64.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       long revNum = 0;
+       int dup = x;
+
+       while(x>0){
+
+        int ld = x %10;
+        revNum = (revNum*10) + ld;
+        x = x/10;
+       }
+
+        int ans = (dup == revNum);
+        return ans;        
+    }
+};
